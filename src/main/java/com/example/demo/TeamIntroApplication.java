5f0f31b9a72763722a76a5528d738e6af407226e
3030936 <3030936@30309-36>
2026-09-16 10:40:24 +0900

    新規

---
+++ src/main/java/com/example/demo/TeamIntroApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeamIntroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeamIntroApplication.class, args);
+	}
+
+}
